d779457143f643056fd4b61dc8c20153d07ba840
mamedun <rikistudio90@gmail.com>
2021-08-16 10:37:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World Mamedun!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
